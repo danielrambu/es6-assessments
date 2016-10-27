1b48bdf56aece536446d066ce279ddc2dca7f008
Daniel Rambu <daniel.rambu@thinslices.com>
2016-10-27 23:15:50 +0300

    Added more tests for template strings and some for arrow functions, block scope and destructuring

--- test/1_templateStrings-basics.js
+++ test/1_templateStrings-basics.js
@@ -1,5 +1,3 @@
-var assert = require('assert');
-
 // 1: template strings - basics
 // To do: make all tests pass, leave the asserts unchanged!
 
@@ -8,7 +6,7 @@ describe('a template string, is wrapped in ` (backticks) instead of \' or "', fu
     describe('by default, behaves like a normal string', function() {
 
         it('just surrounded by backticks', function() {
-            var str = `like a string`;
+            var str = ``;
             assert.equal(str, 'like a string');
         });
 
@@ -20,12 +18,12 @@ describe('a template string, is wrapped in ` (backticks) instead of \' or "', fu
     describe('can evaluate variables, which are wrapped in "${" and "}"', function() {
 
         it('e.g. a simple variable "${x}" just gets evaluated', function() {
-            var evaluated = `x=${x}`;
+            var evaluated = `x=#x`;
             assert.equal(evaluated, 'x=' + x);
         });
 
         it('multiple variables get evaluated too', function() {
-            var evaluated = `${ x }+${ y }`;
+            var evaluated = '${ x } + $ { y }';
             assert.equal(evaluated, x + '+' + y);
         });
 
@@ -34,16 +32,16 @@ describe('a template string, is wrapped in ` (backticks) instead of \' or "', fu
     describe('can evaluate any expression, wrapped inside "${...}"', function() {
 
         it('all inside "${...}" gets evaluated', function() {
-            var evaluated = `${ x  +  y }`;
+            var evaluated = `${ x } + ${ y }`;
             assert.equal(evaluated, x+y);
         });
 
         it('inside "${...}" can also be a function call', function() {
             function getDomain(){
-                return 'github.com';
+                return document.domain;
             }
-            var evaluated = `${ getDomain() }`;
-            assert.equal(evaluated, 'github.com');
+            var evaluated = `${ getDomain }`;
+            assert.equal(evaluated, 'tddbin.com');
         });
 
     });
